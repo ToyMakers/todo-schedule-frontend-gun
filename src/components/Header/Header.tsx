5a1b5d4d83a8eac25d15a77a795b6f9192f1e78f
geon <ehdrjs4046@gmail.com>
2021-07-30 00:12:46 +0900

    [ Add ] : header markup

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Inner from '../common/Inner';
+import Profile from '../Profile/Profile';
+import ProfileBtn from './ProfileBtn';
+
+const HeaderArea = styled.header`
+    height: 43px;
+    /* border-bottom: 1px solid #ccc; */
+    background-color: #000;
+`;
+
+const HeaderInner = styled(Inner)`
+    position: relative;
+`;
+
+const HeaderLogo = styled.div`
+    width: 100%;
+    height: 43px;
+    color: #eee;
+    font-size: 1.1rem;
+    font-weight: 300;
+    text-align: center;
+    line-height: 43px;
+    & strong {
+        font-weight: 500;
+    }
+`;
+
+function Header() {
+    const [profileState, setProfileState] = useState<boolean>(false);
+
+    return (
+        <HeaderArea>
+            <HeaderInner>
+                <HeaderLogo>
+                    <strong>Gune</strong>'s Scheduler
+                </HeaderLogo>
+                <ProfileBtn setProfileState={setProfileState} />
+                <Profile status={profileState} />
+            </HeaderInner>
+        </HeaderArea>
+    );
+}
+
+export default Header;
